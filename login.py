457bb75fe7b1231f3d834a0882decd0d86113bf2
Young Chon <Young@Torins-MacBook-Pro.local>
2020-02-24 21:40:41 -0800

    basic login

--- login.py
+++ login.py
@@ -1,19 +1,46 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
 
-class login():
-    #TODO read from file creds from file
+import os
+import time
+
+class login:
     BASE_URL='https://www.djcity.com'
+    SIGN_IN_URL = BASE_URL + '/signin.aspx'
 
-    def test(self):
-        driver = webdriver.Chrome()
-        driver.maximize_window()
+    def __init__(self, driver):
+        self.d=driver
 
-        driver.get(self.BASE_URL)
+        self.set_credentials()
+        self.signin()
 
-        title = driver.title
+    def set_credentials(self):
+        local_path = os.path.dirname(os.path.realpath('__file__'));
+        filename = os.path.join(local_path, "user.info")
+        file = open(filename, "r")
+        self.user = file.readline()
+        self.pw = file.readline()
 
+
+    def signin(self):
+        self.d.get(self.SIGN_IN_URL)
+        title = self.d.title
         print ("title: {}".format(title))
 
+        user_input= self.d.find_element_by_css_selector("input[name*=UserName]")
+        print user_input
+        user_input.clear()
 
-thing = login()
-thing.test()
+        pass_input= self.d.find_element_by_css_selector("input[type=password]")
+        print pass_input
+        pass_input.clear()
+
+        submit_button = self.d.find_element_by_css_selector("input[type=submit]")
+
+        actions = ActionChains(self.d)
+        actions.send_keys_to_element(user_input, self.user)
+        actions.send_keys_to_element(pass_input, self.pw)
+        actions.click(submit_button)
+        actions.perform()
+        time.sleep(10)
